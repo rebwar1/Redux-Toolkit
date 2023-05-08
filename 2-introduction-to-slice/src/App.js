7eaa0233539eb1e7fc6a2f43a9a5a69ee5f6e60d
rebwar <rebwar1.azizi@gmail.com>
2023-05-08 23:19:27 +0100

    create-todolist-template

--- 2-introduction-to-slice/src/App.js
+++ 2-introduction-to-slice/src/App.js
@@ -2,15 +2,13 @@ import { useState } from 'react';
 import Counter from './components/counter/counter';
 
 function App() {
-  
-  const [counter , setCounter ] = useState(0);
-  const [counterTwo , setCounterTwo ] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
 
-  
   return (
     <div className="App">
-      <Counter counter={counter} setCounter={setCounter}/>
-      <Counter counter={counterTwo} setCounter={setCounterTwo}/>
+      <Counter counter={counter} setCounter={setCounter} />
+      <Counter counter={counterTwo} setCounter={setCounterTwo} />
     </div>
   );
 }
--- 2-introduction-to-slice/src/components/counter/counter.js
+++ 2-introduction-to-slice/src/components/counter/counter.js
@@ -1,15 +1,14 @@
-import IncreaseButton from "./increaseButton"
-import DecreaseButton from "./decreaseButton"
+import IncreaseButton from './increaseButton';
+import DecreaseButton from './decreaseButton';
 
-export default function Counter({ counter , setCounter }) {
-
-    return (
-        <>
-            <h3>Your Number is : {counter}</h3>
-            <div className="flex">
-                <IncreaseButton counter={counter} setCounter={setCounter}/>
-                <DecreaseButton counter={counter} setCounter={setCounter}/>
-            </div>
-        </>
-    )
+export default function Counter({ counter, setCounter }) {
+  return (
+    <>
+      <h3>Your Number is : {counter}</h3>
+      <div className="flex">
+        <IncreaseButton counter={counter} setCounter={setCounter} />
+        <DecreaseButton counter={counter} setCounter={setCounter} />
+      </div>
+    </>
+  );
 }
