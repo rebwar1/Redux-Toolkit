db0356340d3c11074c9a5c5d1fb1936ca5cc2773
rebwar <rebwar1.azizi@gmail.com>
2023-05-11 13:59:29 +0100

    save to localStorage with redux-persist

--- create-todolist-template/src/store/index.js
+++ create-todolist-template/src/store/index.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import todosReducer from './slices/todosSlice';
 
-const store = configureStore({
-  reducer: {
-      todos : todosReducer
-  },
-})
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-export default store;
+const rootReducer = combineReducers({
+  todos: todosReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todos'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
