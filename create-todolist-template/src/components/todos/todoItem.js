d0200aaa8e8f2c38f8c0624caf28af088cbdee0c
rebwar <rebwar1.azizi@gmail.com>
2023-05-11 13:17:18 +0100

    edit text

--- create-todolist-template/src/components/todos/todoItem.js
+++ create-todolist-template/src/components/todos/todoItem.js
@@ -1,4 +1,8 @@
-import { deleteTodo } from '../../store/slices/todosSlice';
+import {
+  deleteTodo,
+  toggleDoneTodo,
+  editTodo,
+} from '../../store/slices/todosSlice';
 import { useDispatch } from 'react-redux';
 
 export default function TodoItem({ todo }) {
@@ -6,6 +10,15 @@ export default function TodoItem({ todo }) {
   const dispatch = useDispatch();
 
   const todoDeleteHandler = () => dispatch(deleteTodo(todo.id));
+  const toggleDoneTodoHandler = () => {
+    dispatch(toggleDoneTodo({ id: todo.id }));
+    dispatch(
+      editTodo({
+        id: todo.id,
+        text: 'new text',
+      })
+    );
+  };
   return (
     <div className="flex mb-4 items-center">
       <p
@@ -17,11 +30,17 @@ export default function TodoItem({ todo }) {
       </p>
 
       {todo.done ? (
-        <button className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-400 border-gray-400 hover:bg-gray-400">
+        <button
+          onClick={toggleDoneTodoHandler}
+          className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-400 border-gray-400 hover:bg-gray-400"
+        >
           Not Done
         </button>
       ) : (
-        <button className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600">
+        <button
+          onClick={toggleDoneTodoHandler}
+          className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600"
+        >
           Done
         </button>
       )}
--- create-todolist-template/src/store/slices/todosSlice.js
+++ create-todolist-template/src/store/slices/todosSlice.js
@@ -1,22 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const todosSlice = createSlice({
-    name : 'todos',
-    initialState : {
-        list : []
+  name: 'todos',
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
     },
-    reducers : {
-        addTodo : (state , action ) => {
-            state.list.push(action.payload)
-        },
-        deleteTodo: (state , action) => {
-            state.list = state.list.filter(todo => todo.id !== action.payload )
-        }
-    }
-})
+    deleteTodo: (state, action) => {
+      state.list = state.list.filter(todo => todo.id !== action.payload);
+    },
+    toggleDoneTodo: (state, { payload }) => {
+      state.list = state.list.map(todo => {
+        return todo.id === payload.id
+          ? {
+              ...todo,
+              done: !todo.done,
+            }
+          : todo;
+      });
+    },
+    editTodo: (state, { payload }) => {
+      state.list = state.list.map(todo => {
+        return todo.id === payload.id
+          ? {
+              ...todo,
+              text: payload.text,
+            }
+          : todo;
+      });
+    },
+  },
+});
 
-
-export const { addTodo , deleteTodo } = todosSlice.actions;
+export const { addTodo, deleteTodo, toggleDoneTodo, editTodo } =
+  todosSlice.actions;
 
 export default todosSlice.reducer;
